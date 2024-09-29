72203270deb77a2ea55dc37888899fe440f28c36
ArthurEnrique15 <arthurenrique1512@gmail.com>
2024-09-29 11:14:52 -0300

    feat: add empty gun shot sound

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -7,31 +7,42 @@ public class Weapon : MonoBehaviour
     public GameObject bulletPrefab;
     public Transform firePoint;
     public float fireForce = 100f;
-    // public float currentAmmo = 6;
+    public float currentAmmo = 6;
     public AudioSource audioSource; // Referência ao AudioSource
     public AudioClip shootSound;    // Referência ao som do tiro
+    public AudioClip emptySound;    // Referência ao som de munição vazia
 
     public void Fire(float rotation)
     {
-        // if (currentAmmo == 0) return;
+        if (currentAmmo > 0)
+        {
+            GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
 
-        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+            Rigidbody2D bulletRb = bullet.GetComponent<Rigidbody2D>();
 
-        Rigidbody2D bulletRb = bullet.GetComponent<Rigidbody2D>();
+            bulletRb.AddForce(firePoint.up * fireForce, ForceMode2D.Impulse);
+            bulletRb.SetRotation(rotation);
 
-        bulletRb.AddForce(firePoint.up * fireForce, ForceMode2D.Impulse);
-        bulletRb.SetRotation(rotation);
+            currentAmmo--;
+        }
 
-        // currentAmmo--;
+        Debug.Log("Current Ammo: " + currentAmmo);
 
         PlayShootSound();
     }
 
     private void PlayShootSound()
     {
-        if (audioSource != null && shootSound != null)
+        if (audioSource != null)
         {
-            audioSource.PlayOneShot(shootSound);
+            if (currentAmmo > 0 && shootSound != null)
+            {
+                audioSource.PlayOneShot(shootSound);
+            }
+            else if (currentAmmo == 0 && emptySound != null)
+            {
+                audioSource.PlayOneShot(emptySound);
+            }
         }
     }
 }
